d7057605961b0001063f80088530e9437b4d9622
KodAgge <aregnell@kth.se>
2022-12-03 22:00:45 +0100

    shortened one liners even more

--- 2/part_1.py
+++ 2/part_1.py
@@ -3,8 +3,8 @@ with open("2/input.txt",'r',encoding = 'utf-8') as file:
     score = 0
     for line in file:
         opp, me = line.strip().split()
-        score += 6 if ord(opp) - ord(me) in [-21, -24] else (3 if ord(opp) - ord(me) == -23 else 0) + 1 if me == "X" else (2 if me == "Y" else 3)        
+        score += (6 if ord(opp) - ord(me) in [-21, -24] else (3 if ord(opp) - ord(me) == -23 else 0)) + (1 if me == "X" else (2 if me == "Y" else 3))
 print(score)
 
 # Alternative 2
-print(sum([6 if ord(m[0])-ord(m[1]) in [-21,-24] else (3 if ord(m[0])-ord(m[1])==-23 else 0)+1 if m[1]=="X" else (2 if m[1]=="Y" else 3) for m in [l.strip().split() for l in open("2/input.txt",'r',encoding='utf-8')]]))
+print(sum([(6 if ord(m[0])-ord(m[1]) in [-21,-24] else (3 if ord(m[0])-ord(m[1])==-23 else 0))+ord(m[1])-87 for m in [l.strip().split() for l in open("2/input.txt",'r',encoding='utf-8')]]))
