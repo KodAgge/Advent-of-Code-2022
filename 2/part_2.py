ccb11222387a11f36c5fc1d285e748c4e29cd301
KodAgge <aregnell@kth.se>
2022-12-03 21:22:27 +0100

    added one liner

--- 2/part_2.py
+++ 2/part_2.py
@@ -1,33 +1,40 @@
+# Alternative 1
 with open("2/input.txt",'r',encoding = 'utf-8') as file:
     score = 0
-#     for line in file:
-#         opp, me = line.strip().split()
-        # score += (ord(me)-ord("X"))*3
-#         (ord("Y")-89)*3
-# #         if me == "Y":
-# #             if opp == "A":
-# #                 score += 4
-# #             if opp == "B":
-# #                 score += 5
-# #             if opp == "C":
-# #                 score += 6
-# #         elif me == "X":
-# #             if opp == "A":
-# #                 score += 3
-# #             if opp == "B":
-# #                 score += 1
-# #             if opp == "C":
-# #                 score += 2
-# #         else:
-# #             if opp == "A":
-# #                 score += 8
-# #             if opp == "B":
-# #                 score += 9
-# #             if opp == "C":
-# #                 score += 7
-# print(score)
-result = (ord("X")-ord("Y"))
+    for line in file:
+        opp, me = line.strip().split()
+        if me == "Y":
+            if opp == "A":
+                score += 4
+            if opp == "B":
+                score += 5
+            if opp == "C":
+                score += 6
+        elif me == "X":
+            if opp == "A":
+                score += 3
+            if opp == "B":
+                score += 1
+            if opp == "C":
+                score += 2
+        else:
+            if opp == "A":
+                score += 8
+            if opp == "B":
+                score += 9
+            if opp == "C":
+                score += 7
+print(score)
 
-# choice = chr(ord("Z") + result)
-# print(choice)
-print(ord("X") - ord("A"))
+# Alternative 2
+with open("2/input.txt",'r',encoding = 'utf-8') as file:
+    score = 0
+    for line in file:
+        opp, me = line.strip().split()
+        match_result = (ord(me) - ord("X")) * 3
+        points_of_choice = ord(opp) + (ord(me)-ord("Y")) * (1 - (3 if (ord(me) - ord(opp) == 23) else 0)) - 64
+        score += match_result + points_of_choice
+print(score)
+
+# Alternative 3
+print(sum([((ord(m[1])-88)*3)+(ord(m[0])+(ord(m[1])-89)*(1-(3 if (ord(m[1])-ord(m[0])==23) else 0))-64) for m in [line.strip().split() for line in open("2/input.txt",'r',encoding='utf-8')]]))
