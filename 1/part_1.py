682b97fe358d22f69f6ceb28d49d092e570bdd21
KodAgge <aregnell@kth.se>
2022-12-03 20:40:08 +0100

    added one liner of part 1

--- 1/part_1.py
+++ 1/part_1.py
@@ -1,11 +1,15 @@
+# ALternative 1
 with open("1/input.txt",'r',encoding = 'utf-8') as file:
-    max = -1
+    max_ = -1
     current = 0
     for line in file:
         if line == "\n":
-            if current > max:
-                max = current
+            if current > max_:
+                max_ = current
             current = 0
         else:
             current += int(line.strip("\n"))
-print(max)
+print(max_)
+
+# Alternative 2
+print(max([sum([int(snack) for snack in elf.split()]) for elf in "".join(["," if line == "\n" else line.strip() + " " for line in open("1/input.txt",'r',encoding = 'utf-8').readlines()]).split(",")]))
