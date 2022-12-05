61e87d9426d972bb041b524ff6c50c28f321526b
August Regnell <48806044+KodAgge@users.noreply.github.com>
2022-12-05 10:20:32 +0100

    added solutions to day 5

---
+++ 5/part_1.py
@@ -0,0 +1,20 @@
+with open("input.txt") as file:
+    piles = [[] for _ in range(9)]
+    indices = [i for i in range(1, 36, 4)]
+    read_piles = True
+    for line in file:
+        if not read_piles:
+            _, n_boxes, _, from_pile, _, to_pile = line.split()
+            n_boxes, from_pile, to_pile = map(int, [n_boxes, from_pile, to_pile])
+            for _ in range(n_boxes):
+                piles[to_pile-1].append(piles[from_pile-1].pop())
+        else:
+            if line =="\n":
+                read_piles = False
+                piles = [pile[::-1] for pile in piles]
+            else:
+                for j,ind in enumerate(indices):
+                    if line[ind] != " " and line[ind] > "9":
+                        piles[j].append(line[ind])
+    top_boxes = "".join([pile[-1] for pile in piles])
+print(top_boxes)
