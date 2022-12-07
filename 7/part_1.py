d7b150e3bbe3483215c8f83d7a2574cc3f706e60
August Regnell <48806044+KodAgge@users.noreply.github.com>
2022-12-07 10:43:19 +0100

    added solutions to day 7

---
+++ 7/part_1.py
@@ -0,0 +1,59 @@
+class Tree:
+    def __init__(self, name = "/", parent = None):
+        self.parent = parent
+        self.name = name
+        self.children = []
+        self.storage = 0
+
+    def __repr__(self) -> str:
+        return f"name: {self.name}, children: {self.children}"
+
+small_dirs = []
+
+def get_small_dirs(node):
+    for child in node.children:
+        get_small_dirs(child)
+    if node.storage <= 100000:
+        small_dirs.append(node.storage)
+
+
+def sum_sub_tree(node):
+    for child in node.children:
+        node.storage += sum_sub_tree(child)
+    return node.storage
+
+def print_tree(node, depth = 0):
+    print('\t'*depth, node.name, node.storage)
+    for child in node.children:
+        print_tree(child, depth + 1)
+
+with open("input.txt") as file:
+    root = Tree()
+    cwd = root
+    read_mode = False
+    for line in file:
+        inputs = line.strip().split()
+        if inputs[0] == "$" and inputs[1] == "cd" and inputs[2] == "/":
+            cwd = root
+        elif inputs[0] == "$" and inputs[1] == "cd" and inputs[2] == "..":
+            cwd = cwd.parent
+        elif inputs[0] == "$" and inputs[1] == "cd" and inputs[2] != "/":
+            index = [dir.name for dir in cwd.children].index(inputs[2])
+            cwd = cwd.children[index]
+        elif inputs[0] == "$" and inputs[1] == "ls":
+            read_mode = True
+        elif read_mode and inputs[0] == "dir":
+            if inputs[1] not in [dir.name for dir in cwd.children]:
+                cwd.children.append(Tree(name=inputs[1], parent=cwd))
+        elif read_mode and inputs[0].isdigit():
+            cwd.storage += int(inputs[0])
+        elif read_mode and inputs[0] == "$":
+            read_mode = False
+
+
+
+print_tree(root)
+sum_sub_tree(root)
+print_tree(root)
+get_small_dirs(root)
+print(sum(small_dirs))
