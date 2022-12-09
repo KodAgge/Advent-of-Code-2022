d3b0272916ca43b6de9c3495d1af04791b5b1f81
August Regnell <48806044+KodAgge@users.noreply.github.com>
2022-12-09 11:02:09 +0100

    saved the correct version

--- 9/part_1.py
+++ 9/part_1.py
@@ -2,33 +2,32 @@ import numpy as np
 
 with open("input.txt") as file:
     tail_positions = set()
-    n_knots = 10
-    positions = [[0, 0] for _ in range(n_knots)]
+    h_position = [0, 0]
+    t_position = [0, 0]
     for line in file:
         direction, steps = line.strip().split()
         for i in range(int(steps)):
             # Move head
             if direction == "R":
-                positions[0][0] += 1
+                h_position[0] += 1
             if direction == "L":
-                positions[0][0] -= 1
+                h_position[0] -= 1
             if direction == "U":
-                positions[0][1] += 1
+                h_position[1] += 1
             if direction == "D":
-                positions[0][1] -= 1
-            
-            # Move other nots
-            for i in range(1, n_knots):
-                x_diff = positions[i-1][0] - positions[i][0]
-                y_diff = positions[i-1][1] - positions[i][1]
-                if abs(x_diff) + abs(y_diff) > 2:
-                    positions[i][0] += np.sign(x_diff)
-                    positions[i][1] += np.sign(y_diff)
-                elif abs(x_diff) > 1:
-                    positions[i][0] += np.sign(x_diff)
-                elif abs(y_diff) > 1:
-                    positions[i][1] += np.sign(y_diff)
+                h_position[1] -= 1
+
+            # Move tail
+            x_diff = h_position[0] - t_position[0]
+            y_diff = h_position[1] - t_position[1]
+            if abs(x_diff) + abs(y_diff) > 2:
+                t_position[0] += np.sign(x_diff)
+                t_position[1] += np.sign(y_diff)
+            elif abs(x_diff) > 1:
+                t_position[0] += np.sign(x_diff)
+            elif abs(y_diff) > 1:
+                t_position[1] += np.sign(y_diff)
 
             # Save position
-            tail_positions.add(str(positions[-1]))
+            tail_positions.add(str(t_position))
     print(len(tail_positions))
