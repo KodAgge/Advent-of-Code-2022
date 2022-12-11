74e21fdc5c01d89c8036980b4fa2d113a993a9b8
KodAgge <aregnell@kth.se>
2022-12-11 18:25:06 +0100

    moved out of folder

--- 8.py
+++ 8.py
@@ -1,16 +1,29 @@
 import numpy as np
 
-with open("input.txt") as file:
+# Part 1
+with open("8.in") as file:
     matrix = []
     for line in file:
         matrix.append(list(map(int, list(line.strip()))))
 
 grid = np.array(matrix)
-scenic_scores = np.zeros(grid.shape)
 w, h = grid.shape
+n_visible = 2 * (w + h) - 4
 
-for i in range(h):
-    for j in range(w):
+for i in range(1,w-1):
+    for j in range(1,h-1):
+        tree_height = grid[i,j]
+        n_visible += np.max(grid[i,:j]) < tree_height or \
+                        np.max(grid[i,j+1:]) < tree_height or \
+                            np.max(grid[:i,j]) < tree_height or  \
+                                np.max(grid[i+1:,j]) < tree_height
+print(f"Answer part 1: {n_visible}")
+
+# Part 2
+scenic_scores = np.zeros(grid.shape)
+
+for i in range(h-1):
+    for j in range(w-1):
         tree_height = grid[i,j]
 
         # Up
@@ -40,4 +53,4 @@ for i in range(h):
         # Save 
         scenic_scores[i,j] += n_trees_up*n_trees_down*n_trees_left*n_trees_right
 
-print(int(np.max(scenic_scores)))
+print(f"Answer part 2: {int(np.max(scenic_scores))}")
